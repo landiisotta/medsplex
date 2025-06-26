ff00f7667f079b2c957af679c9086639c1eab7e3
Isotta Landi <landi.isotta@gmail.com>
2025-06-26 17:58:03 -0400

    added regex validation with pydatic

--- medsplex/chunker.py
+++ medsplex/chunker.py
@@ -2,28 +2,38 @@ import logging
 import re
 from typing import Dict, List
 
+from pydantic import BaseModel, Field, field_validator
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
 
 
-class ConfigFile:
-    pass
+class Regex(BaseModel):
+    regex: str = Field(..., description="Regex pattern for matching keywords.")
+
+    @field_validator("regex")
+    @staticmethod
+    def validate_regex(value: str) -> str:
+        try:
+            re.compile(value)
+        except re.error as e:
+            raise ValueError(f"Invalid regex pattern: {value}. Error: {e}")
+        return value
 
 
-class Chunker:
+class Chunker(BaseModel):
     """
     Helper
     """
 
-    def __init__(self, config: ConfigFile):
-        self._config = config
+    config: Dict[str, Regex]
 
     def run(self, text: str, nchr: int) -> List[Dict]:
         if not isinstance(nchr, int) or nchr <= 0:
             raise Exception(f"nchr should be integer >0, but found {nchr}.")
         chunks = []
-        for keyword, options in self._config.items():
-            pattern = options.get("regex", keyword)
+        for keyword, options in self.config.items():
+            pattern = options.regex
             for match in re.finditer(pattern, text, re.IGNORECASE):
                 start = max(0, match.start() - nchr)
                 end = min(len(text), match.end() + nchr)
--- tests/test_chunker.py
+++ tests/test_chunker.py
@@ -1,4 +1,5 @@
 import pytest
+from pydantic import ValidationError
 
 from medsplex.chunker import Chunker
 
@@ -33,7 +34,7 @@ CONFIG = {
     ),
 )
 def test_run(text, nchr, expected_chunks):
-    chunker = Chunker(CONFIG)
+    chunker = Chunker(config=CONFIG)
     chunks = chunker.run(
         text=text,
         nchr=nchr,
@@ -44,7 +45,7 @@ def test_run(text, nchr, expected_chunks):
 
 @pytest.mark.parametrize("nchr", (0, -1, -100, "foo"))
 def test_run_nchar_is_positive_integer(nchr):
-    chunker = Chunker(CONFIG)
+    chunker = Chunker(config=CONFIG)
     with pytest.raises(Exception) as err:
         chunker.run(
             text="The patient is compliant with the treatment plan. However, there are concerns about their engagement.",
@@ -54,7 +55,7 @@ def test_run_nchar_is_positive_integer(nchr):
 
 
 def test_run_nchar_greater_than_text_length(caplog):
-    chunker = Chunker(CONFIG)
+    chunker = Chunker(config=CONFIG)
     chunks = chunker.run(
         text="The patient is compliant with the treatment plan. However, there are concerns about their engagement.",
         nchr=3000,
@@ -70,3 +71,13 @@ def test_run_nchar_greater_than_text_length(caplog):
         },
     ]
     assert "Text length 101 is less than 3000." in caplog.text
+
+
+def test_chunker_not_valid_regex():
+    with pytest.raises(ValidationError) as err:
+        # This regex is invalid
+        Chunker(config={"compliant": {"regex": "*abc"}})
+    assert (
+        str(err.value)
+        == "1 validation error for Chunker\nconfig.compliant.regex\n  Value error, Invalid regex pattern: *abc. Error: nothing to repeat at position 0 [type=value_error, input_value='*abc', input_type=str]\n    For further information visit https://errors.pydantic.dev/2.11/v/value_error"
+    )
