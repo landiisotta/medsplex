8ee30ed200d895801a211022f70cc3486c4f80df
Isotta Landi <landi.isotta@gmail.com>
2025-08-29 17:44:06 -0400

    added note_id to Chunker input and returned pydantic NoteChunk output + modified all tests

--- tests/test_chunker.py
+++ tests/test_chunker.py
@@ -29,30 +29,37 @@ CONFIG = {
         (
             "The patient is compliant with the treatment plan. However, there are concerns about their engagement.",
             5,
-            [
-                {"keyword": "compliant", "text": "t is compliant with"},
-                {"keyword": "engaged", "text": "heir engagement."},
-            ],
+            {
+                "note_id": 1234,
+                "chunks": [
+                    {"keyword": "compliant", "text": "t is compliant with"},
+                    {"keyword": "engaged", "text": "heir engagement."},
+                ],
+            },
         ),
         (
             "The patient is compliant with the treatment plan. However, there are concerns about their engagement.",
             10,
-            [
-                {"keyword": "compliant", "text": "atient is compliant with the "},
-                {"keyword": "engaged", "text": "out their engagement."},
-            ],
+            {
+                "note_id": 1234,
+                "chunks": [
+                    {"keyword": "compliant", "text": "atient is compliant with the "},
+                    {"keyword": "engaged", "text": "out their engagement."},
+                ],
+            },
         ),
-        ("", 10, []),
+        ("", 10, {"note_id": 1234, "chunks": []}),
     ),
 )
 def test_run(text, nchr, expected_chunks):
     chunker = Chunker(config=CONFIG)
     chunks = chunker.run(
+        note_id=1234,
         text=text,
         nchr=nchr,
     )
-
-    assert chunks == expected_chunks
+    # model_dump() returns a dictionary
+    assert chunks.model_dump() == expected_chunks
 
 
 @pytest.mark.parametrize("nchr", (0, -1, -100, "foo"))
@@ -60,6 +67,7 @@ def test_run_nchar_is_positive_integer(nchr):
     chunker = Chunker(config=CONFIG)
     with pytest.raises(Exception) as err:
         chunker.run(
+            note_id=1234,
             text="The patient is compliant with the treatment plan. However, there are concerns about their engagement.",
             nchr=nchr,
         )
@@ -69,19 +77,23 @@ def test_run_nchar_is_positive_integer(nchr):
 def test_run_nchar_greater_than_text_length(caplog):
     chunker = Chunker(config=CONFIG)
     chunks = chunker.run(
+        note_id=1122,
         text="The patient is compliant with the treatment plan. However, there are concerns about their engagement.",
         nchr=3000,
     )
-    assert chunks == [
-        {
-            "keyword": "compliant",
-            "text": "The patient is compliant with the treatment plan. However, there are concerns about their engagement.",
-        },
-        {
-            "keyword": "engaged",
-            "text": "The patient is compliant with the treatment plan. However, there are concerns about their engagement.",
-        },
-    ]
+    assert chunks.model_dump() == {
+        "note_id": 1122,
+        "chunks": [
+            {
+                "keyword": "compliant",
+                "text": "The patient is compliant with the treatment plan. However, there are concerns about their engagement.",
+            },
+            {
+                "keyword": "engaged",
+                "text": "The patient is compliant with the treatment plan. However, there are concerns about their engagement.",
+            },
+        ],
+    }
     assert "Text length 101 is less than 3000." in caplog.text
 
 
